5b9f30438635958d4b6d01db8b101b923cf0f2c8
xKyousou <31027663+xKyousou@users.noreply.github.com>
2018-04-28 21:06:36 +0800

    completed income report and intermediate one

---
+++ intermediate_two.py
@@ -0,0 +1,14 @@
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45',
+'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole',
+'InterpreterInterface', 'StartServer', 'bob']
+
+user_name = input("Enter your username\n\n>>>")
+check = False
+for name in usernames:
+    if name == user_name:
+        check = True
+
+if check == True:
+    print("Access granted")
+else:
+    print("Access denied")
