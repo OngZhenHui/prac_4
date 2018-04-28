2849b2be0dbd9affeaa7a2d282b6b9b962615aa7
xKyousou <31027663+xKyousou@users.noreply.github.com>
2018-04-28 21:07:14 +0800

    completed intermediate two

--- intermediate_two.py
+++ intermediate_two.py
@@ -1,6 +1,6 @@
 usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45',
-'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole',
-'InterpreterInterface', 'StartServer', 'bob']
+             'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole',
+             'InterpreterInterface', 'StartServer', 'bob']
 
 user_name = input("Enter your username\n\n>>>")
 check = False
